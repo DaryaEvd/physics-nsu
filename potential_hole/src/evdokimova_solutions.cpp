fa7caafd9687c8739ff94be46119f5e3736927e8
Darya Evdokimova <evddaryaevg@gmail.com>
2023-06-23 14:38:53 +0700

    added cmake to hole

--- potential_hole/src/evdokimova_solutions.cpp
+++ potential_hole/src/evdokimova_solutions.cpp
@@ -55,39 +55,15 @@ void drawWithNormScale(std::vector<double> &line) {
   }
 }
 
-void createHole(const int U, const int a, const int amountOfNumbers) {
-  std::vector<double> verticalWall =
-      generatePoints(-U, 0, amountOfNumbers);
-  std::vector<double> leftVertic(amountOfNumbers, 0);
-  std::vector<double> rightVertic(amountOfNumbers, a);
-
-  std::vector<double> cornerLeftX =
-      generatePoints(0, -a, amountOfNumbers);
-  std::vector<double> cornerLeftY(cornerLeftX.size(), 0);
-
-  std::vector<double> bottomX = generatePoints(0, a, amountOfNumbers);
-  std::vector<double> bottomY(bottomX.size(), -U);
-
-  std::vector<double> cornerRightX =
-      generatePoints(a, 2 * a, amountOfNumbers);
-  std::vector<double> cornerRightY(cornerRightX.size(), 0);
-
-  plt::plot(cornerLeftX, cornerLeftY, "blue");
-  plt::plot(leftVertic, verticalWall, "blue");
-  plt::plot(bottomX, bottomY, "blue");
-  plt::plot(rightVertic, verticalWall, "blue");
-  plt::plot(cornerRightX, cornerRightY, "blue");
-
-}
-
 int main(int argc, char **argv) {
   // argv[1] = a - width of hole
   if (argc != 2) {
-    std::cout << "Width of hole is needed" << std::endl;
+    std::cout << "Width of hole is needed as argument to main"
+              << std::endl;
     return 0;
   }
 
-  if (argv[1] < 0) {
+  if (std::atoi(argv[1]) < 0) {
     std::cout << "Error! Width should be > 0" << std::endl;
   }
 
@@ -118,18 +94,30 @@ int main(int argc, char **argv) {
   std::vector<double> intersectionsRightTg =
       findSolutions(amountOfNumbers, epsilon, E, tg, rightPart);
 
+  std::cout << "Intersections for symmetric function: " << std::endl;
+  printOutput(intersectionsRightTg);
+
   std::vector<double> intersectionsRightCtg =
       findSolutions(amountOfNumbers, epsilon, E, ctg, rightPart);
 
-  createHole(U_0, a, amountOfNumbers);
+  std::cout << "Intersections for assymmetric function: "
+            << std::endl;
+  printOutput(intersectionsRightCtg);
 
-  std::vector<double> levelsTan = generatePoints(0, a, amountOfNumbers);
-  for(double solution : intersectionsRightTg) {
-    std::vector<double> numbers(levelsTan.size(), solution);
-    plt::plot(levelsTan, numbers);
-  }
+  std::cout << "===== intersections found ======" << std::endl;
 
+  drawWithNormScale(rightPart);
+  drawWithNormScale(tg);
+  drawWithNormScale(ctg);
+
+  plt::plot(E, rightPart);
+  plt::plot(E, tg);
+  plt::plot(E, ctg);
+
+  plt::plot(E, xAxis);
   plt::show();
+
+  plt::close();
 
   return 0;
 }
